5703459e5cf79d7c470f456caba997900b3b59cd
mabarbar <mateusz.barzenc00@gmail.com>
2022-09-08 17:51:04 +0200

    learning react

---
+++ 02_react/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Contents from "./Contents";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Contents />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 02_react/src/Contents.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
+const Contents = () => {
+  const [name, setName] = useState("Mateusz");
+  const [count, setCount] = useState(0);
+  const handleNameChange = () => {
+    const names = ["Mati", "Paula", "Ola"];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  // const handleClick2 = (name) => {
+  //   console.log(`${name} clicked it`);
+  // };
+
+  const handleClick3 = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {name}</p>
+      <button onClick={handleNameChange}>Change name!</button>
+      <button onClick={handleClick}>Click it!</button>
+      <button onClick={(e) => handleClick3(e)}>Click it!</button>
+    </main>
+  );
+};
+
+export default Contents;
