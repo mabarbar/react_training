efde7d3fd848ea229517f08238a6efdcb6014b2a
mabarbar <mateusz.barzenc00@gmail.com>
2022-09-09 22:14:37 +0200

    learning react

---
+++ 02_react/src/LineItem.js
@@ -0,0 +1,27 @@
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item" key={item.id}>
+      <input
+        type="checkbox"
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+        aria-Label={`Delete ${item.item}`} 
+      ></FaTrashAlt>
+    </li>
+  );
+};
+
+export default LineItem;
